67560fa00b627234f71743439c54b0172a780ff7
Luisa Alzate <110583760+Luisaalzate29@users.noreply.github.com>
2025-08-07 19:06:19 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const dbconnect = ()=>{
-    mongoose.set('strictQuery',true);
-    mongoose.connect("mongodb://127.0.0.1:27017/basedatos7")
-    .then((success)=>console.log("conexion exitosa"))
-    .catch((err)=>console.log(err.message));
-
-}
-module.exports = dbconnect;
